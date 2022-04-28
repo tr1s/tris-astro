fccbcde37a2bf3fe7c3a61e45f59af3f34736bc5
Tristan Lawrence <trislaw@gmail.com>
2022-04-27 22:53:53 -0400

    ✨ lookin' decent... ✨

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -7,7 +7,7 @@ const SectionTitle = () => {
     <section className={styles.projects}>
       <div className="inner-wrapper">
         <div className="marquee-container">
-          <h2 className="marquee">Projectssss</h2>
+          <h2 className="marquee">Projects</h2>
         </div>
         {/* <Marquee play>
           I can be a React component, multiple React components, or just some text.
